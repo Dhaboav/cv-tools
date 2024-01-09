0ac5f49bb8c68a067f37bf7feea02dd3a1224db4
dhaboav <dhaboav@gmail.com>
2024-01-09 22:03:14 +0700

    Combine Main with LabelXml

--- label/Main.py
+++ label/Main.py
@@ -2,6 +2,7 @@ import tkinter.messagebox as msg
 from frontend.Interface import ChoiceDialog
 from backend.TrainRatio import TrainRatio
 from backend.Convert import ConvertXML2YOLO
+from backend.LabelXml import LabelCheckXML
 
 
 if __name__ == "__main__":
@@ -10,7 +11,21 @@ if __name__ == "__main__":
     choice = tester.choice
     folder_path = tester.input_path
 
-    if choice == 'Konversi XML ke YOLO':
+    if choice == 'Cek Label XML':
+        if folder_path:
+
+            # Untuk data class sesuai urutan dari saat melakukan labeling.
+            class_name = ["ROBOT", "BOLA", "PENGHALANG", "GAWANG"]
+            class_color = [(0, 255, 0), (0, 140, 255), (0, 0, 255), (255, 255, 255)]
+            class_count = [0, 0, 0, 0]
+
+            excute = LabelCheckXML(dataset_path=folder_path)
+            # train_boolean --> True: Train, False: Val
+            excute.run(train_boolean=True, class_name=class_name, class_color=class_color, class_counter=class_count)
+        else:
+            msg.showerror('Error', 'No Folder Path!')
+
+    elif choice == 'Konversi XML ke YOLO':
         if folder_path:
             class_mapping = {"0": "ROBOT", "1": "BOLA", "2": "PENGHALANG", "3": "GAWANG"}
             excute = ConvertXML2YOLO(folder_path=folder_path, class_mapping=class_mapping)
--- label/backend/LabelXml.py
+++ label/backend/LabelXml.py
@@ -9,33 +9,17 @@ import sys
 class LabelCheckXML:
     def __init__(self, dataset_path):
         self.dataset_path   = dataset_path
-        self.result_path    = os.path.join('runs', 'label')
-        self.folder_name    = 'exp' 
-        self.folder_path    = self.checking_folder_name()
+        self.output_path    = os.path.join(dataset_path, 'LabelXML')
+        os.makedirs(self.output_path, exist_ok=True)
         self.image_counter  = 0
 
     # Directory Stuff
-    def checking_folder_name(self):
-        folder = os.path.join(self.result_path, self.folder_name)
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-            return folder
-
-        folder_count = 2
-        while True:
-            new_folder_name = f'{self.folder_name}{folder_count}'
-            folder = os.path.join(self.result_path, new_folder_name)
-            if not os.path.exists(folder):
-                os.makedirs(folder)
-                return folder
-            folder_count += 1
-
     def write_label_image(self, image, result_image):
-        image_path = os.path.join(self.folder_path, os.path.basename(image))
+        image_path = os.path.join(self.output_path, os.path.basename(image))
         cv.imwrite(image_path, result_image)
 
     def no_label(self, missing_label):
-        log_file = os.path.join(self.folder_path, 'NoLabel.txt')
+        log_file = os.path.join(self.output_path, 'NoLabel.txt')
         with open(log_file, 'a') as file:
             file.write('\n' + missing_label)
     # ==============================================================================
@@ -49,7 +33,7 @@ class LabelCheckXML:
     def class_plt(self, total, class_name, class_number):
         plt.title(f"Class Distribution of {total} Images")
         plt.bar(class_name, class_number)
-        plt_file = os.path.join(self.folder_path, 'PlotClass')
+        plt_file = os.path.join(self.output_path, 'PlotClass')
         plt.savefig(plt_file)
     # ==============================================================================
 
