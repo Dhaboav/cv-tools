3925f4329c620f288944ae26b4ff5e6b1d83227d
Dhaboav <dhaboav@gmail.com>
2024-02-02 18:46:29 +0700

    Add variable in main Kalibrasi

--- kalibrasi/Main.py
+++ kalibrasi/Main.py
@@ -3,7 +3,7 @@ from frontend.Message import CustomPopup
 from backend.ImageCalibrate import ImageCalibrate
 from backend.RealTimeCalibrate import RealTimeCalibrate
 from backend.Test import Test
-from variable import *
+from variable import CAMERA_INDEX, CAMERA_WIDTH, CAMERA_HEIGHT, CHESSBOARD_SIZE, SQUARE_SIZE
 
 if __name__ == "__main__":
     tester = ChoiceDialog()
@@ -13,19 +13,19 @@ if __name__ == "__main__":
     folder_path = tester.input_path
     if choice == "Test Hasil Kalibrasi":
         if folder_path:
-            excute = Test(0, 640, 480, folder_path)
+            excute = Test(camera=CAMERA_INDEX, width=CAMERA_WIDTH, height=CAMERA_HEIGHT, path=folder_path)
             excute.run()
             
     elif choice == "Kalibrasi real time":
         if folder_path:
-            excute = RealTimeCalibrate(0, (7,9), 23, folder_path=folder_path)
-            excute.detecting_chess_pattern((640, 480))
+            excute = RealTimeCalibrate(camera=CAMERA_INDEX, chessboard_size=CHESSBOARD_SIZE, square_size=SQUARE_SIZE, folder_path=folder_path)
+            excute.detecting_chess_pattern(frame_size=(CAMERA_WIDTH, CAMERA_HEIGHT))
         else:
             box.error_popup("No folder path!")
 
     elif choice == "Kalibrasi gambar":
         if folder_path:
-            excute = ImageCalibrate(chessboard_size=(7, 9), square_size=23, folder_path=folder_path)
+            excute = ImageCalibrate(chessboard_size=CHESSBOARD_SIZE, square_size=SQUARE_SIZE, folder_path=folder_path)
             excute.run()
         else:
             box.error_popup("No folder path!")
