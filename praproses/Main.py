04bbdfae86facbfb84d215509fba7d084e5c75c0
Dhaboav <dhaboav@gmail.com>
2024-02-02 18:52:58 +0700

    Add variable in main Praproses

--- praproses/Main.py
+++ praproses/Main.py
@@ -3,6 +3,7 @@ from frontend.Interface import ChoiceDialog
 from backend.TakeImg import TakeImg
 from backend.ColorSelector import Color
 from backend.ChangeName import ChangeName
+from variable import CAMERA_INDEX, CAMERA_WIDTH, CAMERA_HEIGHT, NAME_FORMATING
 
 
 if __name__ == "__main__":
@@ -13,19 +14,19 @@ if __name__ == "__main__":
 
     if choice == 'Ambil gambar':
         if folder_path:
-            excute = TakeImg(camera_index=0, camera_size=(640,480), folder_path=folder_path)
+            excute = TakeImg(camera_index=CAMERA_INDEX, camera_size=(CAMERA_WIDTH, CAMERA_HEIGHT), folder_path=folder_path)
             excute.run()
         else:
             msg.showerror('Error', 'No Folder Path!')
 
     elif choice == 'Pilih warna':
-        excute = Color(0, 640, 480)
+        excute = Color(camera=CAMERA_INDEX, width=CAMERA_WIDTH, height=CAMERA_HEIGHT)
         excute.run()
 
     elif choice == 'Ubah nama':
         if folder_path:
             # example of name format -> test-{}.jpg
-            excute = ChangeName(folder_path=folder_path, name_format='test-{}.jpg')
+            excute = ChangeName(folder_path=folder_path, name_format=NAME_FORMATING)
             excute.run()
         else:
             msg.showerror('Error', 'No Folder Path!')
